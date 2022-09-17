968777888000739448a3f9d34634e056c5d3e0dc
Fernanda Guerra <60495755+Ff3rn4nd4@users.noreply.github.com>
2022-09-17 00:28:28 -0500

    Add files via upload

---
+++ DatosExtra.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApiZapateria.Entidades
+{
+    public class DatosExtra
+    {
+        //esta entidad guardará datos temporales tales como la temporada de los zapatos y su precio
+        public int Id { get; set; }
+        public string Temporada { get; set; }
+        public int Precio { get; set; }
+    }
+}
---
+++ Zapato.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApiZapateria.Entidades
+{
+    public class Zapato
+    {
+        public int Id {get;set;}
+        //datos: id, color, material, talla 
+        public int Codigo { get; set; }
+        public string Tipo { get; set; }
+        public string Marca { get; set; }
+        public string Color { get; set; }
+        public string Material { get; set; }
+        public int Talla { get; set; }
+    }
+}
